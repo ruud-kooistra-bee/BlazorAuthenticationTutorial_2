277e2a2861f563ac9809f93823132a71a6effb34
ruud-kooistra-bee <ruud.kooistra.bee@outlook.com>
2022-03-10 14:31:47 +0100

    Add project files.

---
+++ BlazorAuthenticationTutorial_2/Client/Program.cs
@@ -0,0 +1,17 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Blazored.LocalStorage;
+
+using BlazorAuthenticationTutorial_2.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
---
+++ BlazorAuthenticationTutorial_2/Server/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using BlazorAuthenticationTutorial_2.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAuthenticationTutorial_2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<string>> Login(UserLoginDto request)
+        {
+            string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWFyb24gU3dhcnR6IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiTWFzdGVyIEhhY2tlciIsImV4cCI6MTY0Njk5MTA4MX0.yNvdCnbntwFppT-jQxay413hnds3-AD6LmEeeR-Iavs-gcv9uIdy3WuVMy-8cgAPs5WgNvNJPWCsNRn-IuX9xw";
+            return token;
+        }
+    }
+}
